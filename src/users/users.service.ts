cab308ef2ac6dcb3b41cbcb211de67fab20ca2c8
toan.nguyen <toan.nguyenhuu@asnet.com.vn>
2025-10-06 14:32:58 +0700

    feat: implement users crud, closes #16

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,14 +1,101 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateUserDto, UpdateUserDTO } from './dto';
+import { hashPassword } from 'src/shared/utils';
 
 @Injectable()
 export class UsersService {
   constructor(private prisma: PrismaService) {}
 
-  async getUser(email: string) {
-    return this.prisma.user.findUnique({
+  async findOne(identifier: string) {
+    const isEmail = identifier.includes('@');
+
+    return await this.prisma.user.findUnique({
+      where: isEmail ? { email: identifier } : { id: identifier },
+    });
+  }
+
+  async getUserByEmail(email: string) {
+    return await this.findOne(email);
+  }
+
+  async getUserById(id: string) {
+    return await this.findOne(id);
+  }
+
+  async findAll({ page, limit }: { page: number; limit: number }) {
+    const skip = (page - 1) * limit;
+
+    const [total, users] = await this.prisma.$transaction([
+      this.prisma.user.count(),
+      this.prisma.user.findMany({
+        skip,
+        take: limit,
+        orderBy: {
+          createdAt: 'desc',
+        },
+        select: {
+          id: true,
+          email: true,
+          firstName: true,
+          lastName: true,
+          createdAt: true,
+          updatedAt: true,
+        },
+      }),
+    ]);
+
+    const lastPage = Math.ceil(total / limit);
+    const nextPage = page + 1 > lastPage ? null : page + 1;
+
+    return {
+      data: users,
+      meta: {
+        currentPage: page,
+        totalPages: lastPage,
+        itemsPerPage: limit,
+        totalItems: total,
+        lastPage,
+        nextPage,
+      },
+    };
+  }
+
+  async create(dto: CreateUserDto) {
+    // Check if email already exists
+    const existingUser = await this.prisma.user.findUnique({
+      where: { email: dto.email },
+    });
+
+    if (existingUser) {
+      throw new ConflictException('Email already registered');
+    }
+
+    const hassedPassword = await hashPassword(dto.password);
+
+    return this.prisma.user.create({
+      data: {
+        ...dto,
+        password: hassedPassword,
+      },
+    });
+  }
+
+  async update(id: string, dto: UpdateUserDTO) {
+    return this.prisma.user.update({
       where: {
-        email,
+        id,
+      },
+      data: {
+        ...dto,
+      },
+    });
+  }
+
+  async delete(id: string) {
+    return this.prisma.user.delete({
+      where: {
+        id,
       },
     });
   }
