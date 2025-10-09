22674b2c6828721d2a781e991430b46171fa30a9
toan.nguyen <toan.nguyenhuu@asnet.com.vn>
2025-10-09 10:22:04 +0700

    fix: resolve dependency injection issue

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
 
 describe('UsersController', () => {
   let controller: UsersController;
@@ -7,6 +8,19 @@ describe('UsersController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
+      providers: [
+        {
+          provide: UsersService,
+          useValue: {
+            getUserByEmail: jest.fn(),
+            getUserById: jest.fn(),
+            findAll: jest.fn(),
+            create: jest.fn(),
+            update: jest.fn(),
+            delete: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
