f27ab035157ef9336c14e31fedc96e4ef450eced
toan.nguyen <toan.nguyenhuu@asnet.com.vn>
2025-10-09 15:27:23 +0700

    feat: write auth unit test, closes #31

---
+++ src/mocks/auth.ts
@@ -0,0 +1,7 @@
+export const MOCK_ACCESS_TOKEN = 'access_token';
+export const MOCK_REFRESH_TOKEN = 'refresh_token';
+
+export const MOCK_CREDENTIALS = {
+  email: 'toan.nguyenhuu@asnet.com.vn',
+  password: 'password123',
+};
