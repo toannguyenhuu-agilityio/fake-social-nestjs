6d835531f58da3f532dc48ef938a73bb2c015006
toan.nguyen <toan.nguyenhuu@asnet.com.vn>
2025-10-09 18:16:27 +0700

    feat: write users unit test, closes #33

--- src/mocks/user.ts
+++ src/mocks/user.ts
@@ -7,3 +7,22 @@ export const MOCK_USER = {
   createdAt: new Date('2025-10-09T00:00:00.000Z'),
   updatedAt: new Date('2025-10-09T00:00:00.000Z'),
 };
+
+export const MOCK_LIST_USER = [
+  {
+    id: '1',
+    email: 'toan.nguyenhuu@asnet.com.vn',
+    firstName: 'Toan',
+    lastName: 'Nguyen',
+    createdAt: new Date('2025-10-09T00:00:00.000Z'),
+    updatedAt: new Date('2025-10-09T00:00:00.000Z'),
+  },
+  {
+    id: '2',
+    email: 'toan.nguyenhuu@asnet.com.vn',
+    firstName: 'Toan',
+    lastName: 'Nguyen',
+    createdAt: new Date('2025-10-09T00:00:00.000Z'),
+    updatedAt: new Date('2025-10-09T00:00:00.000Z'),
+  },
+];
